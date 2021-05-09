1f6d92e30452790fc523b0206da77f1645e20138
Alexandria-dr <sasha110druz@gmail.com>
2021-05-09 20:12:13 +0300

    new sections

--- js/script.js
+++ js/script.js
@@ -16,3 +16,24 @@ closeButton.addEventListener('click', e => {
     document.querySelector('.tools__content').classList.toggle('hide');
     (closeButton.innerHTML == '<i class="fas fa-times"></i>')? closeButton.innerHTML = '<i class="far fa-arrow-alt-circle-down"></i>' : closeButton.innerHTML = '<i class="fas fa-times"></i>'
 })
+
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    autoHeight: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: "fraction",
+      formatFractionCurrent: addZero,
+      formatFractionTotal: addZero,
+    
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      type: 'custom'
+    },
+  });
+
+  function addZero(num){
+    return (num > 9) ? num : '0' + num;
+  }
