a7098e8d7a620443a28869e640d768578fade9fb
Alexandria-dr <sasha110druz@gmail.com>
2021-05-09 12:21:34 +0300

    +section

--- js/script.js
+++ js/script.js
@@ -5,3 +5,14 @@ navButton.addEventListener('click', e => {
     document.querySelector('.nav__menu_mobile').classList.toggle('active');
     document.querySelector('body').classList.toggle('lock')
 })
+
+
+const closeButton = document.querySelector('.btn__close');
+
+closeButton.addEventListener('click', e => {
+    e.preventDefault();
+    closeButton.classList.toggle('active');
+    document.querySelector('.tools__text').classList.toggle('active');
+    document.querySelector('.tools__content').classList.toggle('hide');
+    (closeButton.innerHTML == '<i class="fas fa-times"></i>')? closeButton.innerHTML = '<i class="far fa-arrow-alt-circle-down"></i>' : closeButton.innerHTML = '<i class="fas fa-times"></i>'
+})
