a7a60089813792939f0715a58c2806703701001b
Alexandria-dr <sasha110druz@gmail.com>
2021-05-10 18:05:33 +0300

    new

--- js/script.js
+++ js/script.js
@@ -6,16 +6,62 @@ navButton.addEventListener('click', e => {
     document.querySelector('body').classList.toggle('lock')
 })
 
+var container = document.querySelector('.tools__content');
+container.style.height = container.clientHeight + 'px';
 
-const closeButton = document.querySelector('.btn__close');
+var linkToggle = document.querySelector('.btn__close');
 
-closeButton.addEventListener('click', e => {
-    e.preventDefault();
-    closeButton.classList.toggle('active');
+  linkToggle.addEventListener('click', function(event){
+    event.preventDefault();
+    var container = document.querySelector('.tools__content');
+    (linkToggle.innerHTML == '<i class="fas fa-times"></i>')? linkToggle.innerHTML = '<i class="far fa-arrow-alt-circle-down"></i>' : linkToggle.innerHTML = '<i class="fas fa-times"></i>'
     document.querySelector('.tools__text').classList.toggle('active');
-    document.querySelector('.tools__content').classList.toggle('hide');
-    (closeButton.innerHTML == '<i class="fas fa-times"></i>')? closeButton.innerHTML = '<i class="far fa-arrow-alt-circle-down"></i>' : closeButton.innerHTML = '<i class="fas fa-times"></i>'
-})
+    if (!container.classList.contains('hide')) {
+  
+    container.style.height = '0px';
+    container.classList.add('hide');
+      
+    } else {
+      container.style.height = 'auto';
+      var height = container.clientHeight + 30 + 'px';
+      container.style.height = '0px';
+
+      setTimeout(function () { 
+        container.style.height = height ;
+      }, 0);
+      container.classList.remove('hide');
+      
+    }
+  });
+
+
+  var linkTogglee = document.querySelectorAll('.js-toggle');
+for(i = 0; i < linkTogglee.length; i++){
+  linkTogglee[i].addEventListener('click', function(event){
+    event.preventDefault();
+    var container = document.getElementById(this.dataset.container);
+    this.classList.toggle('active');
+    if (!container.classList.contains('active')) {
+      container.classList.add('active');
+      container.style.height = 'auto';
+      var height = container.clientHeight + 'px';
+      container.style.height = '0px';
+      setTimeout(function () {
+        container.style.height = height;
+      }, 0);
+    } else {
+      container.style.height = '0px';
+      container.addEventListener('transitionend', function () {
+        container.classList.remove('active');
+      }, {
+        once: true
+      });
+    }
+  });
+}
+
+
+
 
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1,
